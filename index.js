2ee66888e3094922c9e998db2aee61d8e7011471
kogotti@yahoo.com <kogotti@yahoo.com>
2018-08-06 19:18:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ titanic.addEventListener("click", function(event) {
   document.getElementById("director").innerHTML=movies["Titanic"]["director"]
   document.getElementById("genre").innerHTML=movies["Titanic"]["genre"]
   document.getElementById("filmRating").innerHTML=movies["Titanic"]["filmRating"]
-  // document.getElementById("poster").innerHTML=movies["Titanic"]["poster"]
+  document.getElementById("poster").src=movies["Titanic"]["poster"]
   document.getElementById("audienceScore").innerHTML=movies["Titanic"]["audienceScore"]
   document.getElementById("description").innerHTML=movies["Titanic"]["description"]
 })
