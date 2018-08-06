62e2c67b7af17d1b620b8d821321ae5453428904
kogotti@yahoo.com <kogotti@yahoo.com>
2018-08-06 19:08:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ titanic.addEventListener("click", function(event) {
   console.log("I was clicked")
   document.getElementById("title").innerHTML= movies["Titanic"]["title"]
   document.getElementById("director").innerHTML=movies["Titanic"]["director"]
+  document.getElementById("genre"),innerHTML=movies["Titanic"]["genre"]
   document.getElementById()
 })
   
