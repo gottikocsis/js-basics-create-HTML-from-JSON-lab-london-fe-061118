01f0824c823de639fb6ffb5d50aede973c74d517
kogotti@yahoo.com <kogotti@yahoo.com>
2018-08-06 19:02:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentloaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
-}
+   const titanic = document.getElementById('Titanic')
+   console.log(titanic)
+ 
+  titanic.addEventListener("click", function(event) {
+    document.getElementById("title").innerHTML=movies["Titanic"]["title"]
+  })
+  
+})
+
+
