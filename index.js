0a9085a87289d36b8473aebfc97af02b4fbf1dbf
kogotti@yahoo.com <kogotti@yahoo.com>
2018-08-06 19:33:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ titanic.addEventListener("click", function(event) {
   
   let i;
   for (i = 0; i < cast.length; i++) { 
-    text += cast[i] + "<br>";
+    text += cast[i][name] + "<br>";
    
 }
   
