7b42753bb5b0f0e3b3bd3c11dd0a429aa4684294
kogotti@yahoo.com <kogotti@yahoo.com>
2018-08-06 19:31:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ titanic.addEventListener("click", function(event) {
   let i;
   for (i = 0; i < cast.length; i++) { 
     text += cast[i] + "<br>";
-    // var role,
-    //   translateValue = jsonKeys[i][key];
+   
 }
   
 })
