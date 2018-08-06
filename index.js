f406759b0801b84d415934ff3d4b1634aff5fb22
kogotti@yahoo.com <kogotti@yahoo.com>
2018-08-06 19:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ titanic.addEventListener("click", function(event) {
   
   let i;
   for (i = 0; i < cast.length; i++) { 
-    cast = movies.Titanic.cast[i].actor + "<br>";
+    actor = movies.Titanic.cast[i].actor + "<br>";
 }
   
 })
