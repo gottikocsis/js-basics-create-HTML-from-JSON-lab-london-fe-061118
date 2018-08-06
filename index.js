f725b660a2fb77bc9e1fd49a7536c7af28fb9fcb
kogotti@yahoo.com <kogotti@yahoo.com>
2018-08-06 19:05:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const titanic = document.getElementById('Titanic')
 console.log(titanic)
  
 titanic.addEventListener("click", function(event) {
+  console.log("I was clicked")
   document.getElementById("title").innerHTML= movies["Titanic"]["title"]
 })
   
